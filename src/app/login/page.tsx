4344fdd7b8bb13e6a58e0615f8ba31931e602271
Bokang <bokangngoetjana06@gmail.com>
2025-06-11 13:57:33 +0200

    feature: added ant design forms for login and register

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react"
+import LoginForm from "@/components/auth/loginForm"
+
+export default function LoginPage() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react"
+import RegisterForm from "@/components/auth/registerForm"
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <h1>Register Page</h1>
+      <RegisterForm />
+    </div>
+  );
+}
