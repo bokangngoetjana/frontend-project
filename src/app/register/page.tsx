8209f57295f8d5a8eeb7a0f798dc319f68cd1b33
Bokang <bokangngoetjana06@gmail.com>
2025-06-13 09:25:20 +0200

    chore: added register form styling

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,24 @@
 'use client'
 import React from "react"
+import { ConfigProvider } from "antd";
 import RegisterForm from "@/components/auth/registerForm"
 
 export default function RegisterPage() {
   return (
-    <div>
+    <ConfigProvider theme={{
+      token: {
+        colorPrimary: "5a2a7f",
+        colorBgBase: "#fefbff",
+        colorTextBase: "4a285f",
+        borderRadius: 8,
+        fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
+      },
+    }}>
+<div>
       <h1>Register Page</h1>
       <RegisterForm />
     </div>
+    </ConfigProvider>
+    
   );
 }
